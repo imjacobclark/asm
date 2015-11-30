4dca5602028962df831279981022c5744664c6e5
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-11-30 22:38:03 +0000

    Updating comments

--- src/errcode.s
+++ src/errcode.s
@@ -2,6 +2,6 @@
 
 .globl _start
 _start:
-    mov     r0, $204 	/* Set exit code to 204 in register r0 */
-    mov     r7, $1 	/* exit is syscall 1, store in register r7 */
-    swi     $0 		/* Invoke the system call */
+    mov     r0, $204      /* Set exit code to 204 in register r0 */
+    mov     r7, $1      /* Load syscall number into register r7 (1 for exit) */
+    swi     $0          /* Invoke the system call */
