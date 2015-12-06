b12216eadf7fd0bf54c1bbe4268eca2e92f81e42
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-12-06 23:51:19 +0000

    Add with added memory

--- src/add.s
+++ src/add.s
@@ -1,9 +1,23 @@
+.data
+
+.balign 4
+dnum1:
+	.word	5
+.balign 4
+dnum2:
+	.word 	10
+
 .text
 .global _start
 _start:
-    mov 	r0, $5
-    mov 	r1, $6
+    ldr 	r0, num1
+    ldr		r0, [r0]
+    ldr 	r1, num2
+    ldr		r1, [r1]    
     add 	r0, r0, r1
 
     mov 	r7, $1
     swi 	$0
+
+num1 : .word dnum1
+num2 : .word dnum2
