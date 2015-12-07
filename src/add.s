5a5a3d131488e67e5170ad8feef6315c9df80057
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-12-07 08:58:49 +0000

    Add

--- src/add.s
+++ src/add.s
@@ -1,22 +1,17 @@
 .data
-dnum1:
+num1:
 	.word	5
-dnum2:
+num2:
 	.word 	10
 
 .text
 .global _start
 _start:
-    ldr 	r0, num1
-    ldr		r0, [r0]
-    ldr 	r1, num2
-    ldr		r1, [r1]    
+    ldr 	r0, =num1
+    ldr         r0, [r0]
+    ldr 	r1, =num2
+    ldr         r1, [r1]
     add 	r0, r0, r1
 
     mov 	r7, $1
     swi 	$0
-
-num1: 
-	.word dnum1
-num2: 
-	.word dnum2
